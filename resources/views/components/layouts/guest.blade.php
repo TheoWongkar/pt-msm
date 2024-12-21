9613473574838fd5fbba955d04da89f64f4940c3
TheoWongkar <theoterra19@gmail.com>
2024-12-21 10:27:19 +0800

    Membuat components/layouts

---
+++ resources/views/components/layouts/guest.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="{ darkMode: localStorage.getItem('darkMode') === 'true' }" x-init="$watch('darkMode', val => localStorage.setItem('darkMode', val))"
+    :class="{ 'dark': darkMode }">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        PT. MSM/TTN
+        @isset($title)
+            | {{ $title }}
+        @endisset
+    </title>
+
+    <link rel="icon" href="img/msm-logo.svg" type="image/x-icon">
+
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @endif
+</head>
+
+<body>
+
+    <main>
+        {{ $slot }}
+    </main>
+
+</body>
+
+</html>
